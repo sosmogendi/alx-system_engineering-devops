24d6a88dee22b0ff55dd340eb2da7e99a1eaad82
Sospeter <sosmogendi@gmail.com>
2023-08-01 07:49:02 -0700

    Correct regular expression to exclude "hbn" from matching

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+def match_pattern(input_string)
+  # Use the regular expression with repetition tokens to match the patterns
+  matched = input_string.match(/hb(t+)n/)
+  matched ? matched[0] : ""
+end
+
+# Main script
+if __FILE__ == $PROGRAM_NAME
+  input_string = ARGV[0]
+  result = match_pattern(input_string)
+  puts result
+end
