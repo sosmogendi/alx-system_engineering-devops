2dcf9dc24c15334fde68c9e1700dc1fe67f74737
Sospeter <sosmogendi@gmail.com>
2023-10-23 12:15:34 -0700

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+This script fetches information about employee TODO lists from a REST API and exports it to a JSON file.
+Usage: python3 3-dictionary_of_list_of_dictionaries.py
+"""
+
+import json
+import requests
+
+def fetch_employee_todo_data():
+    base_url = "https://jsonplaceholder.typicode.com"
+    users_url = f"{base_url}/users"
+
+    try:
+        users_response = requests.get(users_url)
+        users_data = users_response.json()
+
+        todo_data = {}
+
+        for user in users_data:
+            user_id = user["id"]
+            user_name = user["username"]
+            tasks_url = f"{base_url}/todos?userId={user_id}"
+            tasks_response = requests.get(tasks_url)
+            tasks_data = tasks_response.json()
+
+            todo_data[str(user_id)] = []
+
+            for task in tasks_data:
+                todo_data[str(user_id)].append({
+                    "username": user_name,
+                    "task": task["title"],
+                    "completed": task["completed"]
+                })
+
+        with open("todo_all_employees.json", "w") as json_file:
+            json.dump(todo_data, json_file)
+
+        print("Data exported to todo_all_employees.json")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    fetch_employee_todo_data()
