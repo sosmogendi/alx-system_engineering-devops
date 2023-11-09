c3cefe34b984620100508aa1d68784b6c9114cc5
Sos <sosmogendi@gmail.com>
2023-11-09 10:15:54 +0000

    Strace analysis

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,6 @@
-# This is a comment explaining what the Puppet manifest is about
-class my_module::strace_fix {
+# Fix bad `phpp` extension to `php`
 
-  # Use the exec resource to run a command that fixes the issue
-  exec { 'fix-wordpress':
-    command     => 'your-fixing-command-here',  # Replace with the actual command to fix the issue
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    refreshonly => true,
-  }
-
-  # Define a service resource to ensure Apache is running
-  service { 'apache2':
-    ensure => running,
-  }
-
-  # Make the service dependent on the exec resource
-  Service['apache2'] -> Exec['fix-wordpress']
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Apply the class to fix the issue
-include my_module::strace_fix
